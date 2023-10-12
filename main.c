e613ee0cab6a8ab4eb66daad29cc776ae499383a
amennad <amennad@student.42nice.fr>
2023-10-12 15:45:22 +0200

    feat (prompt): add prompt to minishell

--- main.c
+++ main.c
@@ -6,14 +6,28 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 09:11:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/10/12 11:07:06 by amennad          ###   ########.fr       */
+/*   Updated: 2023/10/12 15:44:17 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void generate_prompt(void)
+{
+	char *user;
+	char *prompt;
+
+	user = getenv("USER");
+	user = ft_strjoin(user, " $> ");
+	while (1)
+	{
+		prompt = readline(user);
+		printf("%s\n", prompt);
+	}
+}
+
 int	main(void)
 {
-	ft_printf("Hello World\n");
+	generate_prompt();
 	return (0);
 }
