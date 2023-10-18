b3a880ed1b89ec520c569c9aa261c2330cfb8cb7
amennad <amennad@student.42nice.fr>
2023-10-18 12:45:59 +0200

    feat (lexer) : bracket <, feat OK, norme OK, maybe need more test

--- lexer/create_lexer_list.c
+++ lexer/create_lexer_list.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:07:48 by amennad           #+#    #+#             */
-/*   Updated: 2023/10/18 12:26:01 by amennad          ###   ########.fr       */
+/*   Updated: 2023/10/18 12:45:40 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,8 +60,34 @@ int	is_right_bracket(t_msh *msh, char *prompt, int *i)
 			return (258);
 		}
 		else
-			lexer_push(msh, ">", W_APPEND_REDIRECT);
-		*i += 2;
+			lexer_push(msh, ">>", W_APPEND_REDIRECT);
+		*i += 1;
+	}
+	return (0);
+}
+
+int	is_left_bracket(t_msh *msh, char *prompt, int *i)
+{
+	if (prompt[*i + 1] != '<' && prompt[*i + 1] != '|')
+		lexer_push(msh, "<", R_REDIRECT);
+	if (prompt[*i + 1] == '|')
+	{
+		exit_synthax_error(msh, "|");
+		return (258);
+	}
+	if (prompt[*i + 1] == '<')
+	{
+		if (prompt[*i + 2] == '<' || prompt[*i + 2] == '|')
+		{
+			if (prompt[*i + 2] == '<')
+				exit_synthax_error(msh, "<");
+			else if (prompt[*i + 2] == '|')
+				exit_synthax_error(msh, "|");
+			return (258);
+		}
+		else
+			lexer_push(msh, "<<", HEREDOC);
+		*i += 1;
 	}
 	return (0);
 }
@@ -82,6 +108,8 @@ int	lexer_create_list(t_msh *msh, char *prompt)
 			return_code = is_blank(msh, prompt, &i);
 		else if (prompt[i] == '>')
 			return_code = is_right_bracket(msh, prompt, &i);
+		else if (prompt[i] == '<')
+			return_code = is_left_bracket(msh, prompt, &i);
 		if (return_code != 0)
 		{
 			msh->return_code = return_code;
