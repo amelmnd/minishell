33e46b6883be5f4481b752682c778979ca8e09d0
amennad <amennad@student.42nice.fr>
2023-10-20 12:10:32 +0200

    feat (lexer) : feat cara with not special car is wold, norme OK, maybe need more test

--- lexer/create_lexer_list.c
+++ lexer/create_lexer_list.c
@@ -6,7 +6,7 @@
 /*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:07:48 by amennad           #+#    #+#             */
-/*   Updated: 2023/10/18 18:07:05 by amennad          ###   ########.fr       */
+/*   Updated: 2023/10/20 12:05:02 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ int	is_lexer_cara(t_msh *msh, char *prompt, int *i, char c)
 		return_code = is_double_quote(msh, prompt, i);
 	else if (c == '$')
 		return_code = is_dollar(msh, prompt, i);
+	else
+		return_code = is_word(msh, prompt, i);
 	return (return_code);
 }
 
---
+++ lexer/is_word.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_word.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 12:03:23 by amennad           #+#    #+#             */
+/*   Updated: 2023/10/20 12:09:26 by amennad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	is_world_end(char *prompt, int i)
+{
+	int	world_len;
+
+	world_len = 0;
+	while (prompt[i] && prompt[i] != ' ' && prompt[i] != '\t'
+		&& prompt[i] != '\0' && prompt[i] != '>' && prompt[i] != '<'
+		&& prompt[i] != '>' && prompt[i] != '<'
+		&& prompt[i] != '|' && prompt[i] != '$')
+	{
+		world_len++;
+		i++;
+	}
+	return (world_len);
+}
+
+int	is_word(t_msh *msh, char *prompt, int *i)
+{
+	int		world_len;
+	char	*world;
+
+	world_len = is_world_end(prompt, *i);
+	world = ft_substr(prompt, *i, world_len);
+	lexer_push(msh, world, WORD);
+	*i += world_len - 1;
+	return (0);
+}
