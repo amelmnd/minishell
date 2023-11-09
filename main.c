74140baf49ecdf0e9c08c0ff895f53c92db6295f
Nociception <nicolas.stoutzer@hotmail.fr>
2023-11-09 10:43:28 +0100

    read the comment in the main concerning return_code

--- main.c
+++ main.c
@@ -6,11 +6,13 @@
 /*   By: nstoutze <nstoutze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 09:11:01 by amennad           #+#    #+#             */
-/*   Updated: 2023/11/08 22:29:45 by nstoutze         ###   ########.fr       */
+/*   Updated: 2023/11/09 09:50:41 by nstoutze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int print = 1;
 
 void	generate_prompt(char *envp[])
 {
@@ -23,6 +25,11 @@ void	generate_prompt(char *envp[])
 	build_user_for_prompt(msh);
 	while (42)
 	{
+		if (print) {printf("generate_prompt(while) : début itération ; msh->return_code = %d\n", msh->return_code);}
+		//msh->return_code est conservé d'une itération à la suivante (ce que l'on veut)
+		// une instruction dans la suite de la boucle réassigne la valeur à 0 avant l'expander
+		// ce qui empêche de pouvoir l'afficher avec un echo $? par exemple
+		
 		//generate_msh_env(msh); // ne pas supprimer
 		//assigne à msh->(char **)msh_env un char ** généré à partir de msh->env_list
 		// qui est modifiée et conservée au fur et à mesure de cette boucle (generate_prompt)
