ad85a12a55770db555f0f8716aaffecfb65185fe
amelmnd <amelmnd.dev@gmail.com>
2023-11-16 16:46:01 +0100

    feat (builtin) : add pwd

--- builtins/pwd_builtin.c
+++ builtins/pwd_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd_builtin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nstoutze <nstoutze@student.42.fr>          +#+  +:+       +#+        */
+/*   By: amennad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:27:55 by nstoutze          #+#    #+#             */
-/*   Updated: 2023/11/14 14:36:45 by nstoutze         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:44:10 by amennad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 void	pwd_builtin(t_msh *msh, t_exec_list *exec_list_node)
 {
-	(void)msh;
+
 	(void)exec_list_node;
+
+	t_env_list	*env_list;
+
+	env_list = msh->env_list;
+	while (env_list)
+	{
+		if (ft_strcmp(env_list->name, "PWD"))
+		{
+			printf("%s\n", env_list->value);
+			break ;
+		}
+		env_list = env_list->next;
+	}
 }
