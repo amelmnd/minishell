e3df7ba07ecab6c9f72fa0dbc8aac80d5ece1c54
Nociception <nicolas.stoutzer@hotmail.fr>
2023-11-24 20:39:37 +0100

    compiles ; echo fixed

--- utils/generic/ft_strcmp.c
+++ utils/generic/ft_strcmp.c
@@ -6,12 +6,14 @@
 /*   By: nstoutze <nstoutze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 12:38:23 by nstoutze          #+#    #+#             */
-/*   Updated: 2023/11/23 12:38:24 by nstoutze         ###   ########.fr       */
+/*   Updated: 2023/11/24 20:23:10 by nstoutze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//return 1 if s1 and s2 are the same
+//return 0 otherwise
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	i;
