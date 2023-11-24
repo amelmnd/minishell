0564792a19e512e2a667e9e81793d9dca346e545
Nociception <nicolas.stoutzer@hotmail.fr>
2023-11-24 17:22:20 +0100

    compiles ; unset problemS fixed
    unset does not make valgrind or fsanitize angry anymore
    unset PATH does not lead to a segfault anymore ; and builtins still work without PATH

--- builtins/echo_builtin.c
+++ builtins/echo_builtin.c
@@ -6,13 +6,12 @@
 /*   By: nstoutze <nstoutze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 14:24:25 by nstoutze          #+#    #+#             */
-/*   Updated: 2023/11/24 17:18:26 by nstoutze         ###   ########.fr       */
+/*   Updated: 2023/11/24 17:20:49 by nstoutze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
 int	ft_array_size(char **array)
 {
 	int	i;
@@ -42,21 +41,15 @@ int	ft_flag_n_cmp(char *s1)
 
 void	echo_builtin(t_msh *msh, t_exec_list *exec_list_node)
 {
-	dprintf(2, "echo_builtin : Entrée ; msh->return_code = %d\n", msh->return_code);
 	int		i;
 	t_bool	n_flag;
 	int		array_size;
 	int		compare;
 
+	(void)msh;
 	i = 1;
 	n_flag = FALSE;
-	if (exec_list_node->nb_words == 1)
-	{
-		printf("\n");
-		return ;
-	}
 	array_size = ft_array_size(exec_list_node->args_array);
-	dprintf(2, "echo_builtin : PROBE ; msh->return_code = %d\n", msh->return_code);
 	compare = ft_flag_n_cmp(exec_list_node->args_array[1]);
 	if (compare)
 		n_flag = TRUE;
@@ -71,6 +64,4 @@ void	echo_builtin(t_msh *msh, t_exec_list *exec_list_node)
 	}
 	if (n_flag == FALSE)
 		printf("\n");
-	msh->return_code = 0;
 }
-*/
